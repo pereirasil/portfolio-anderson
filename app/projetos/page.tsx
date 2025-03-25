f179b34b383649757b3d79cd36ef09284a6e072d
pereirasil <anderson.informata@gmail.com>
2025-03-25 02:13:37 -0300

    git commit -m "Página de Projetos em manutenção"

---
+++ app/projetos/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Navbar from "../../components/Navbar";
+
+export default function Projetos() {
+  return (
+    <div className="min-h-screen bg-gray-100 text-black flex flex-col">
+      <Navbar />
+      <div className="flex-grow flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-center leading-relaxed">
+          Tela de projetos está em manutenção, <br /> mas em breve vai estar disponível.
+        </h1>
+      </div>
+    </div>
+  );
+}
